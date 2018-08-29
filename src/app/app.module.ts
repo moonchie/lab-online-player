d319873bfd4516c208cd1511ee3d99a501c1eed8
shanx2 <shanshan.dev@gmail.com>
2018-08-29 23:22:50 +0200

    update HTTP and apid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,11 @@ import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +25,11 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,25 +3,61 @@ import { Player } from './player';
 import { PLAYERS } from './mock-players';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageSerivce: MessageService) { }
+  private playersUrl = 'api/players';  // URL to web api
+
+  constructor(
+    private http: HttpClient,
+    private messageSerivce: MessageService) { }
 
   getPlayers(): Observable<Player[]>{
-    // still use the mock data from the ts file
-        // TODO: send the message _after_ fetching the players
-    this.messageSerivce.add("I got a player service for you!");
-    return of (PLAYERS);
+
+    return this.http.get<Player[]>(this.playersUrl)
+      .pipe(
+        // if error
+        catchError(this.handleError('getPlayers', []))
+      )
   }
 
 
   getPlayer(id: number): Observable<Player>{
-    // TODO: send the message _after_ fetching the hero
-    this.messageSerivce.add(`Fetched hero id = ${id}`);
-    return of (PLAYERS.find(player => player.id === id))
+
+    const url = `${this.playersUrl}/${id}`;
+    return this.http.get<Player>(url).pipe(
+      tap(_ => this.log(`fetched player id=${id}`)),   // <------------- use message service
+      catchError(this.handleError<Player>(`getPlayer id=${id}`))
+    );
   }
+
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+private handleError<T> (operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+
+    // TODO: better job of transforming error for user consumption
+    this.log(`${operation} failed: ${error.message}`);
+
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
+}
+
+private log(message: string) {
+  this.messageSerivce.add(`HeroService: ${message}`);
+}
+
 }
